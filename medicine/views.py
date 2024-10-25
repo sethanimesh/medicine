f06604793af4849aa2f5af93cbb12fd4ed6263e4
animesh <sethanimesh@hotmail.com>
2024-10-25 14:48:34 +0530

    #2 loading the template in views

--- medicine/views.py
+++ medicine/views.py
@@ -1,9 +1,13 @@
+from unittest import loader
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
 from medicine.models import Medicine
 
 # Create your views here.
 def index(request):
     medicine_list = Medicine.objects.all()
-    return HttpResponse(medicine_list)
+    template = loader.get_template('medicine/index.html')
+    context = {'medicine_list':medicine_list,}
+    return HttpResponse(template.render(context, request))
