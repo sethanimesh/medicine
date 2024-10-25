0fae288eec3d85015acf4dc69be576d80e78560a
animesh <sethanimesh@hotmail.com>
2024-10-25 14:29:14 +0530

    #2 creating a user and registering rows

--- medicine/admin.py
+++ medicine/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from medicine.models import Medicine, UserMedication
+
 # Register your models here.
+admin.site.register(Medicine)
+admin.site.register(UserMedication)
