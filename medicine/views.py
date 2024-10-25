138cee41013a46227361502e938f49ced2293856
animesh <sethanimesh@hotmail.com>
2024-10-25 14:37:50 +0530

    #2 modifying to view table contents

--- medicine/views.py
+++ medicine/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from medicine.models import Medicine
+
 # Create your views here.
 def index(request):
-    return HttpResponse("hello World")
+    medicine_list = Medicine.objects.all()
+    return HttpResponse(medicine_list)
